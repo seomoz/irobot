aaaff9890eac5851941f715c8ad472904ed19cb1
Jason Younker <jason@ynkr.org>
2014-04-15 11:18:17 -0700

    namespace null object to avoid collisions

--- lib/null_object.rb
+++ lib/null_object.rb
@@ -1,24 +1,26 @@
-class NullObject
-  def method_missing(*args, &block)
-    nil
+module Irobot
+  class NullObject
+    def method_missing(*args, &block)
+      nil
+    end
+
+    def present?
+      false
+    end
+
+    def nil?
+      true
+    end
+
+    def to_ary
+      []
+    end
   end
 
-  def present?
-    false
-  end
-
-  def nil?
-    true
-  end
-
-  def to_ary
-    []
-  end
-end
-
-def Maybe(value)
-  case value
-  when nil then NullObject.new
-  else value
+  def Maybe(value)
+    case value
+    when nil then NullObject.new
+    else value
+    end
   end
 end
